d09d28cbd1787deaa6f85bcd7a3843b43d017a6c
FeatherWhite <tomw5823@outlook.com>
2019-11-25 08:34:43 +0800

    实验

---
+++ projects/CsharpFundamentals/jin/Chapter_6/UnitTest/UnitTest/ClassLibTestProject/MyClassTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using ClassLib;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ClassLibTestProject
+{
+    [TestClass]
+    public class MyClassTest
+    {
+        [TestMethod]
+        public void TestMethod()
+        {
+            //准备
+            MyClass testMethod = new MyClass();
+            //设定测试用例
+            int value = 1;
+            int expected = 2;
+            //执行
+            int actual = testMethod.DoubleValue(value);
+            //断言
+            Assert.AreEqual(expected, actual);
+            //实验
+        }
+    }
+}
