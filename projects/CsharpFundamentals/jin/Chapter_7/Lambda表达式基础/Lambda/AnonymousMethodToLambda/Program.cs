af16e8511b21160d8992009d7f50393a4e16f2ae
FeatherWhite <tomw5823@outlook.com>
2019-11-26 09:07:44 +0800

    匿名方法到lambda表达式

---
+++ projects/CsharpFundamentals/jin/Chapter_7/Lambda表达式基础/Lambda/AnonymousMethodToLambda/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnonymousMethodToLambda
+{
+    //定义委托
+    public delegate int AddDelegate(int x, int y);
+    class MyClass
+    {
+        private int Add(int x,int y)
+        {
+            return x + y;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //使用C#匿名方法特性直接给委托变量赋值
+            AddDelegate addDel = delegate (int x, int y)
+            {
+                return x + y;
+            };
+            //通过委托变量调用匿名委托
+            int result = addDel(5,10);
+            Console.WriteLine(result);
+            //直接将匿名方法作为函数参数
+            InvokeDelegate(delegate (int x, int y)
+            {
+                return x + y;
+            }, 5, 10);
+            //使用lambda表达式
+            InvokeDelegate((x, y) => x + y, 10, 20);
+            Console.ReadLine();
+        }
+        //使用委托类型参数的方法
+        public static void InvokeDelegate(AddDelegate addDel, int x, int y)
+        {
+            Console.WriteLine(addDel(x, y)); 
+        }
+    }
+}
---
+++ projects/CsharpFundamentals/jin/Chapter_7/Lambda表达式基础/Lambda/TypicalDelegateExample/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TypicalDelegateExample
+{
+    //1.定义一个委托
+    delegate int AddDelegate(int x, int y);
+    //2.定义一个MyClass类，放置一个满足AddDelegate委托要求的方法
+    class MyClass
+    {
+        public int Add(int x,int y)
+        {
+            return x + y;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //3.定义一个委托变量
+            AddDelegate addDel = null;
+            //4.实例化一个MyClass对象，并将其Add方法引用传给委托变量del
+            MyClass myClass = new MyClass();
+            addDel = myClass.Add;
+            //5.通过委托变量调用MyClass对象的Add()方法
+            int result = addDel(5, 10);
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
