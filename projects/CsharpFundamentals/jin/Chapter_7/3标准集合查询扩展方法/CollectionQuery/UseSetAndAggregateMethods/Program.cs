4e0238698b193e0251a708351d45c571816fc600
FeatherWhite <tomw5823@outlook.com>
2019-11-29 18:42:23 +0800

    常用扩展方法

---
+++ projects/CsharpFundamentals/jin/Chapter_7/3标准集合查询扩展方法/CollectionQuery/UseSetAndAggregateMethods/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UseSetAndAggregateMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random ran = new Random();
+            int[] array = new int[10];
+            for (int i = 0; i < 10; i++)
+            {
+                array[i] = ran.Next(1, 100);
+                Console.Write(array[i]+", ");
+            }
+            Console.WriteLine();
+            Console.WriteLine("总和{0}",array.Sum());
+            Console.WriteLine("平均值{0}",array.Average());
+            Console.WriteLine("最大值{0}",array.Max());
+            Console.WriteLine("最小值{0}",array.Min());
+            Console.ReadLine();
+        }
+    }
+}
